741c304943bc58300d32858f101d28a25e402c38
Ishihara <c0b22011b7@edu.teu.ac.jp>
2023-11-21 15:45:09 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -16,6 +16,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])#練習4
+        screen.blit(im_ls[tmr%2],[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
