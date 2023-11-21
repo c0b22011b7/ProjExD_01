674b600c2d2a8baede87ff5771badfe5e389ee14
Ishihara <c0b22011b7@edu.teu.ac.jp>
2023-11-21 16:34:49 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -9,15 +9,17 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kt_img = pg.image.load("ex01/fig/3.png")#lec2
     kt_img = pg.transform.flip(kt_img,True,False)#lec2
-    im_ls=[kt_img,pg.transform.rotozoom(kt_img,10,1.0)]
+    im_ls=[pg.transform.rotozoom(kt_img,i,1.0) for i in range(11)]
+    count=[0,1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1,0]
+    print(len(count))
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x =tmr%1600
+        x =tmr%1600        
         screen.blit(bg_img, [-x, 0])#練習4
         screen.blit(bg_img,[1600-x,0])
-        screen.blit(im_ls[tmr%2],[300,200])
+        screen.blit(im_ls[count[tmr%20]],[300,200])        
         pg.display.update()
         tmr += 1        
         clock.tick(100)
