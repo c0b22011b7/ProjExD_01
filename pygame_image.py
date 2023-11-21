d49a4ec371ea7a25300c321a50b03bc6a17645f3
Ishihara <c0b22011b7@edu.teu.ac.jp>
2023-11-21 15:34:29 +0900

    練習2

---
+++ pygame_image.py
@@ -0,0 +1,27 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    kt_img = pg.image.load("ex01/fig/3.png")#lec2
+    kt_img = pg.transform.flip(kt_img,True,False)#lec2
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])#練習4
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
