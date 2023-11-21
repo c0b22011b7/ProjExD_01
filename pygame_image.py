963be278aaaa044d6e043a0f0cff113d44d70ba6
Ishihara <c0b22011b7@edu.teu.ac.jp>
2023-11-21 17:13:08 +0900

    ensyuu1-2

--- pygame_image.py
+++ pygame_image.py
@@ -12,14 +12,15 @@ def main():
     kt_img = pg.transform.flip(kt_img,True,False)#lec2
     im_ls=[pg.transform.rotozoom(kt_img,i,1.0) for i in range(11)]
     count=[0,1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1,0]
-    print(len(count))
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x =tmr%1600        
+        x =tmr%3200 
+        print(x)      
         screen.blit(bg_img, [-x, 0])#練習4
         screen.blit(bg2_img,[1600-x,0])
+        screen.blit(bg_img,[3200-x,0])
         screen.blit(im_ls[count[tmr%20]],[300,200])        
         pg.display.update()
         tmr += 1        
